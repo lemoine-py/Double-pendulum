5367e4752c03452b82fa63767853b695cb2f8ffe
Duck-coder <slshenron@duck.com>
2024-12-09 11:31:07 +0100

    update after TP1-S13

--- Python codes/lyapunov.py
+++ Python codes/lyapunov.py
@@ -1,22 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 28 16:59:44 2024
+""" 
+1. Jacobian funtion
+2. RK4 for matrices
+3. SPIN-UP phase -> 3.1 via RK4 for delta_x(t)
+                    3.2 via scipy.linalg.expm
 
-@author: Solal
-"""
+SPIN-UP phase for the double pendulum system"""
 
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
+# Parameters
 l1 = 1
 l2 = 1
 m1 = 1
 m2 = 1
 g = 9.81
 
-### Jacobian ### -------------------------------------------------------------------------------------
-def jacobian(w1, w2,th1, th2):
+### Defining the jacobian matrix ### -------------------------------------------------------------------------------------
+def jacobian(w1, w2, th1, th2):
     jac = np.zeros((4,4))
     
     jac[0][2] = 1 #dth1/dw1
@@ -44,129 +46,9 @@ def jacobian(w1, w2,th1, th2):
                                                                                    
     return jac
 
-### Runge-kutta 4 for matrices
-
-def rk4_matrix(dt,N,A):
-    y = np.zeros((4, N+1))
-    y[:, 0] = np.array([0, 0, 10**(-10), 0]) # delta's initial conditions
-    for i in range(N):
-        k1 = dt * (A @ y[:, i])
-        k2 = dt * (A @ (y[:, i] + k1/2))
-        k3 = dt * (A @ (y[:, i] + k2/2))
-        k4 = dt * (A @ (y[:, i] + k3))
-        y[:, i+1] = y[:, i] + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
-    return y
-
-### Spin-up phase ### -------------------------------------------------------------------------------------
-
-# initial conditions
-w1_0 = 0
-w2_0 = 0
-th1_0 = 0.1
-th2_0 = np.pi
-
-n = 10000
-t_max = 10000
-dt = 1
-t_delta = np.linspace(0, t_max, n)
-
-delta_0 = [0, 0, 10**(-10), 0]
-
-J = jacobian(w1_0, w2_0, th1_0, th2_0)
-
-#-------------------------------------------
-delta = rk4_matrix(dt, n, J)
-
-def lya_exp(t, delta):
-    lya = np.zeros(n)
-    norm = np.zeros(n)
-    for i in range (n):
-        norm[i] = np.linalg.norm(delta[:,i])
-    
-    for i in range(n):
-        lya[i] = np.log((norm[i])/norm[0])/t[i]
-    return lya
-
-lya = lya_exp(t_delta, delta)
-#-------------------------------------------
-delta_a = np.zeros((n,4))
-for i in range(n):
-    delta_a[i] = sp.linalg.expm(J*t_delta[i]) @ delta_0
-
-def lya_a_exp(t, delta):
-    lya = np.zeros(n)
-    norm = np.zeros(n)
-    for i in range (n):
-        norm[i] = np.linalg.norm(delta[i])
-    
-    for i in range(n):
-        lya[i] = np.log((norm[i])/norm[0])/t[i]
-    return lya
-
-lya_a = lya_a_exp(t_delta, delta_a)
-#-------------------------------------------
-
-plt.figure()
-plt.plot(t_delta, lya)
-plt.plot(t_delta, lya_a)
-plt.suptitle("Lyapunov exponent")
-plt.xlabel("Time (s)")
-plt.ylabel("Lyapunov exponent")
-plt.grid()
-plt.show()
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 28 16:59:44 2024
-
-@author: Solal
-"""
-
-import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
-
-l1 = 1
-l2 = 1
-m1 = 1
-m2 = 1
-g = 9.81
-
-
-
-### Jacobian ### -------------------------------------------------------------------------------------
-def jacobian(w1, w2,th1, th2):
-    jac = np.zeros((4,4))
-    
-    jac[0][2] = 1 #dth1/dw1
-    jac[1][3] = 1 #dth2/w2
-    jac[2][2] = -2*np.sin(th1-th2)*m2*2*w1*l1*np.cos(th1-th2)/(l1*(2*m1+m2-m2*np.cos(2*th1-2*th2)))
-    jac[2][3] = -2*np.sin(th1-th2)*m2*2*w2*l2/(l1*(2*m1+m2-m2*np.cos(2*th1-2*th2)))
-    jac[3][2] = 2*np.sin(th1-th2)*2*w1*l1*(m1+m2)/(l2*(2*m1+m2-m2*np.cos(2*th1-2*th2)))
-    jac[3][3] = 2*np.sin(th1-th2)*2*w2*l2*m2*np.cos(th1-th2)/(l2*(2*m1+m2-m2*np.cos(2*th1-2*th2)))
-    
-    num20 = (-g*(2*m1+m2)*np.cos(th1)-m2*g*np.cos(th1-2*th2)-2*np.cos(th1-th2)*m2*w2**2*l2-2*m2*np.cos(th1-th2)**2*w1**2*l1+2*np.sin(th1-th2)**2*m2*w1**2*l1)*l1*(2*m1+m2-m2*np.cos(2*th1-2*th2))-(-g*(2*m1+m2)*np.sin(th1)-m2*g*np.sin(th1-2*th2)-2*np.sin(th1-th2)*m2*(w2**2*l2+w1**2*l1*np.cos(th1-th2)))*(l1*m2*2*np.sin(2*th1-2*th2))
-    denom20 = (l1*(2*m2+m2-m2*np.cos(2*th1-2*th2)))**2
-    jac[2][0] = num20/denom20
-    
-    num21 = (2*m2*g*np.cos(th1-2*th2)+2*np.cos(th1-th2)*m2*w2**2*l2+2*np.cos(th1-th2)**2*m2*l2*w1**2-2*np.sin(th1-th2)**2*m2*w1**2*l1)*l1*(2*m1+m2-m2*np.cos(2*th1-2*th2))-(-g*(2*m1+m2)*np.sin(th1)-m2*g*np.sin(th1-2*th2)-2*np.sin(th1-th2)*m2*(w2**2*l2*w1**2*l2*np.cos(th1-th2)))*(-l1*m2*2*np.sin(2*th1-2*th2))
-    denom21 = (l1*(2*m1+m2-m2*np.cos(2*th1-2*th2)))**2
-    jac[2][1] = num21/denom21
-    
-    num30 = 2*np.cos(th1-th2)*w1**2*l1*(m1+m2)+2*g*(m1+m2)*(np.cos(th1-th2)*np.cos(th1)-np.sin(th1-th2)*np.sin(th1)+2*w2**2*l2*m2*(np.cos(th1-th2)**2-np.sin(th1-th2)**2))*l2*(2*m1+m2-m2*np.cos(2*th1-2*th2))-(2*np.sin(th1-th2)*(w1**2*l1*(m1+m2)+g*(m1+m2)*np.cos(th1)+w2**2*l2*m2*np.cos(th1-th2)))*l1*(2*m2*np.sin(2*th1-2*th2))
-    denom30 = (l2*(2*m1+m2-m2*np.cos(2*th1-2*th2)))**2
-    jac[3][0] = num30/denom30
-    
-    num31 = -2*np.cos(th1-th2)*(w1**2*l1*(m1+m2)+g*(m1+m2)*np.cos(th1))+2*w2**2*l2*m2*m2*(np.sin(th1-th2)**2-np.cos(th1-th2**2))*l1*(2*m1+m2-m2*np.cos(2*th1-2*th2))-(2*np.sin(th1-th2)*(w1**2*l1*(m1+m2)+g*(m1+m2)*np.cos(th1)+w2**2*l2*m2*np.cos(th1-th2)))*(-2*l2*m2*np.sin(2*th1-2*th2))
-    denom31 = (l2*(2*m1+m2-m2*np.cos(2*th1-2*th2)))**2
-    jac[3][1] = num31/denom31
-                                                                                   
-    return jac
-
-### Runge-kutta 4 for matrices
-
-def rk4_matrix(dt,N,A, delta_0):
+### Runge-kutta 4 for matrices ###
+def rk4_matrix(dt, N, A, delta_0):
+    """ Runge-Kutta 4 scheme for F = A@y """
     y = np.zeros((4, N+1))
     y[:, 0] = delta_0 # delta's initial conditions
     for i in range(N):
@@ -177,56 +59,55 @@ def rk4_matrix(dt,N,A, delta_0):
         y[:, i+1] = y[:, i] + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
     return y
 
-### Spin-up phase ### -------------------------------------------------------------------------------------
+### SPIN-UP phase ### -------------------------------------------------------------------------------------
 
-# initial conditions
+# Arbitrary initial conditions
 w1_0 = 0
 w2_0 = 0
-th1_0 = 0.1
-th2_0 = 0.1
+th1_0 = 1.5
+th2_0 = 0
 
 # Time parameters
-n = 100
-t_max = 100
-dt = 1
-t_delta = np.linspace(0, t_max, n)
+N = 5000
+t_max = 50
+dt = 0.01
+t_delta = np.linspace(0, t_max, N) # time array
 
-# Initial DeltaX
-delta_0 = [0, 0.01, 0, 0.01]
+# Arbitrary initial Delta_X
+delta_0 = [0, 0, 10**(-10), 0 ]
 
-# Jacobian
+# Calling the jacobian function and fixing initial values
 J = jacobian(w1_0, w2_0, th1_0, th2_0)
 
 #-------------------------------------------
-### Solving deltaX(t) with RK4
-delta = rk4_matrix(dt, n, J, delta_0)
+### Solving delta_X(t) with RK4
+delta = rk4_matrix(dt, N, J, delta_0)
 
 def lya_exp(t, delta):
-    lya = np.zeros(n)
-    norm = np.zeros(n)
-    for i in range (n):
+    lya = np.zeros(N)
+    norm = np.zeros(N)
+    for i in range (N):
         norm[i] = np.linalg.norm(delta[:,i])
-    
-    for i in range(n):
+    for i in range(N):
         lya[i] = np.log((norm[i])/norm[0])/t[i]
     return lya
 
 lya = lya_exp(t_delta, delta)
 #-------------------------------------------
-### Solving deltaX(t) with matrix exponential
-# !!! order of theta and omega are not the same !!! -> issue yet to be resolved
-delta_a = np.zeros((n,4))
-for i in range(n):
+### Solving delta_X(t) with matrix exponentials
+delta_a = np.zeros((N,4))
+
+for i in range(N):
     delta_a[i] = sp.linalg.expm(J*t_delta[i]) @ delta_0
 
-def lya_a_exp(t, delta):
-    lya = np.zeros(n)
-    norm = np.zeros(n)
-    for i in range (n):
-        norm[i] = np.linalg.norm(delta[i])
+def lya_a_exp(t_delta, delta_a):
+    lya = np.zeros(N)
+    norm = np.zeros(N)
+    for i in range (N):
+        norm[i] = np.linalg.norm(delta_a[i])
     
-    for i in range(n):
-        lya[i] = np.log((norm[i])/norm[0])/t[i]
+    for i in range(N):
+        lya[i] = np.log((norm[i])/norm[0])/t_delta[i]
     return lya
 
 lya_a_exp = lya_a_exp(t_delta, delta_a)
--- Python codes/lyapunov_exponent.py
+++ Python codes/lyapunov_exponent.py
@@ -1,30 +1,28 @@
+""" This code aims to compute the Lyapunov exponents of the doubel pendulum system """
+
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-# Parameters
 m1 = 1
 m2 = 1
 l1 = 1
 l2 = 1
 g = 9.81
 
-# Time parameters
 t_max = 20
-h = 0.01 
-N = int(np.floor(t_max/h))+1 # about 2000 timesteps
+h = 0.01 # Not smaller than that for the gif
+N = int(np.floor(t_max/h))+1
 
-# Initial conditions
 w1_0 = 0
 w2_0 = 0
+
 th1_0 = 1.5
 th2_0 = 0
-u0 = np.array([w1_0, w2_0, th1_0, th2_0])
 
-# Initial delta_x
-#dx0 = np.random.rand(4)
-dx0 = np.array([0, 0, 1, 0])
-dx0 = dx0/(np.linalg.norm(dx0)*10**10) # norm(dx0) = 10**(-10)
+u0 = np.array([w1_0, w2_0, th1_0, th2_0])
+dx0 = np.random.rand(4)
+dx0 = dx0/(np.linalg.norm(dx0)*10**10)
 u0d = u0 + dx0
 
 def F_MIT(w1, w2, th1, th2): # MIT version of the derivative
@@ -41,10 +39,9 @@ def F_MIT(w1, w2, th1, th2): # MIT version of the derivative
     
     
 def solve_RK4(f, u0, h, t_max):
-    """ Returns : t (time array), u (4x1 array of last computed values)"""
     N = int(np.floor(t_max / h)) + 1
     t = np.linspace(0, t_max, N)
-    u = np.zeros(4) # u does not store each set of 4 values for each timestep ! Only the last computed set
+    u = np.zeros(4)
     u = u0
     for i in range(N - 1):
         k1 = h * f(u[0], u[1], u[2], u[3])
@@ -53,6 +50,8 @@ def solve_RK4(f, u0, h, t_max):
         k4 = h * f(u[0] + k3[0], u[1] + k3[1], u[2] + k3[2], u[3] + k3[3])
         u = u + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
     return t, u
+
+#t, u = solve_RK4(big_F, u0, h, t_max)
 
 t1, u1 = solve_RK4(F_MIT, u0, h, t_max)
 t2, u1d = solve_RK4(F_MIT, u0d, h, t_max)
@@ -67,6 +66,7 @@ th2d = u1d[:,3]
 w1d = u1d[:,0]
 w2d = u1d[:,1]
 """
+
 
 def jacobian(th1, th2, w1, w2):
     jac = np.zeros((4,4))
@@ -86,21 +86,39 @@ def jacobian(th1, th2, w1, w2):
 
 
 def lyapunov(u, dx):
-    """ Computing the lyapunov exponent at t_max starting from a given u0 and delta_x"""
-    t = 20 # = t_max = 20 sec
-    M = jacobian(u[2],u[3],u[0],u[1]) # evaluating the jacobian at the 20th timestep
-    dxf = sp.linalg.expm(M*t) @ dx # solving for delta_x
+    N = 20
+    M = jacobian(u[2],u[3],u[0],u[1])
+    dxf = sp.linalg.expm(M*N) @ dx
     norm = np.linalg.norm(dxf)
-    lyapf = np.log(norm/np.linalg.norm(dx))*1/N # Lyapunov exponent of the 20th timestep
+    lyapf = np.log(norm/np.linalg.norm(dx))*1/N
     return lyapf, dxf
 
-# SPIN-UP
-t_max = 100
+th = np.linspace(0,np.pi,30)
+lya_th = np.zeros(30)
+for p in range(30):
+    u0 = np.array([0, 0, th[p], th[p]])
+    lya = np.zeros(50)
+    for i in range(50):
+        t1, u1 = solve_RK4(F_MIT, u0, h, t_max)
+        lya[i], dx = lyapunov(u0, dx0)
+        u0 = u1
+        dx0 = dx/(np.linalg.norm(dx)*10**10)
+    lya_th[p] = np.average(lya)
+plt.plot(th, lya_th, label = "Lyapunov exponent")
+plt.suptitle("Theta-theta initial conditions")
+plt.ylabel("Lyapunov")
+plt.xlabel("Theta (rad)")
+plt.legend()
+plt.grid()
+plt.show()
+    
+
+
+"""
 N = 100
-th1_0 = 1.5
-M = jacobian(th1_0,0,0,0)
+M = jacobian(3,0,0,0)
 dx_0 = [0, 0, 10**(-10), 0]
-t = np.linspace(0,t_max,N)
+t = np.linspace(0,N,N)
 dx = np.zeros((N,4))
 for i in range(N):
     dx[i] = sp.linalg.expm(M*t[i]) @ dx_0
@@ -110,44 +128,12 @@ lyap = np.zeros(N)
 for i in range(N):
     norm[i] = np.linalg.norm(dx[i])
     lyap[i] = np.log(norm[i]/norm[0])*1/t[i]
-
-plt.figure()
-plt.plot(t,lyap, label = f"lyap[-1] = {lyap[-1]}")
-plt.suptitle(f"SPIN-UP for theta1 = {th1_0}")
-plt.ylabel("Lyapunov exponent")
-plt.xlabel("Time (s)")
-plt.legend()
-plt.grid()
+plt.plot(t,lyap)
 plt.show()
 
 # Verifying with theoretical values of lyapunov_max #
 eigenvalue, eigenvector = np.linalg.eig(M)
 print()
 print(eigenvalue)
-print(f"lyap[-1] = {lyap[-1]}")
 print()
-
-
-#-------------------------------------------
-### Theta-theta initial conditions ###
-th = np.linspace(0,np.pi,30) # Angle array
-lya_th = np.zeros(30) # "Lyapunov for each angle" array
-
-for p in range(30):
-    u0 = np.array([0, 0, th[p], th[p]])
-    lya = np.zeros(50)
-    for i in range(50):
-        t1, u1 = solve_RK4(F_MIT, u0, h, t_max) # updating th1, th2, w1, w2
-        lya[i], dx = lyapunov(u0, dx0)
-        u0 = u1 # updating initial conditions
-        dx0 = dx/(np.linalg.norm(dx)*10**10) # Renormalising so that norm(dx0) = 10**(-10)
-    lya_th[p] = np.average(lya)
-
-plt.figure()
-plt.plot(th, lya_th, label = "Lyapunov exponent average")
-plt.suptitle("Theta-theta initial conditions")
-plt.ylabel("Lyapunov")
-plt.xlabel("Theta (rad)")
-plt.legend()
-plt.grid()
-plt.show()
+"""
